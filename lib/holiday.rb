76243ced5e9962858ab42ffe9ca50f954ff47d2a
Kat B <katreesebarnes@gmail.com>
2019-02-21 01:20:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,8 +60,14 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+    holiday_hash.each do |season, holidays|
+    puts "#{season.to_s.capitalize}:"
+      holidays.each do | holiday, supplies|
     
+      
+      puts "  #{holiday.to_s.split('_').map{ |w|  w.capitalize}.join(' ')}:"
+      end
+    end
 end
 
 def all_holidays_with_bbq(holiday_hash)
