2a1a40be8f035acf77c8927ab25fff6b57c6d657
Kat B <katreesebarnes@gmail.com>
2019-02-21 01:45:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
     holiday_hash.each do |season, holiday|
       holiday.each do |holiday_name, supply|
-      symbols << holiday_name if supply.includes?("BBQ")
+      symbols << holiday_name if supply.include?("BBQ")
       end
     end
    symbols
